d1a02e916b0c82c3a091bb77514c485234d6c02e
Rommel Rico <me@rommelrico.com>
2018-05-25 08:40:07 -0700

    Initial commit

---
+++ NewsFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsFun
+//
+//  Created by Rommel Rico on 5/25/18.
+//  Copyright © 2018 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,17 @@
+# NewsFun
+
+A simple app to get the News and experiment with CoreML. 
+
+## Contributing
+
+If you wish to contribute to this repo, please read the [Contributing document](.github/CONTRIBUTING.md).
+
+## Support
+
+If you need help with this project, please read the [Support document](.github/SUPPORT.md).
+
+## License
+
+[MIT License](LICENSE.md)
+
+
