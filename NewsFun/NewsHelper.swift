bf043193734e0561fe35b281912a89770144a16b
Rommel Rico <me@rommelrico.com>
2018-05-25 19:30:53 -0700

    Adding the NewsHelper

---
+++ NewsFun/NewsHelper.swift
@@ -0,0 +1,18 @@
+//
+//  NewsHelper.swift
+//  NewsFun
+//
+//  Created by Rommel Rico on 5/25/18.
+//  Copyright © 2018 Rommel Rico. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class NewsHelper {
+    
+    func getArticles() {
+        
+    }
+    
+}
