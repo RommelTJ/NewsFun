376eb18611f6cadb1f4c1651b04d9b6cd3a1dc59
Rommel Rico <me@rommelrico.com>
2018-05-26 19:02:50 -0700

    Parsing the Article JSON

--- NewsFun/NewsHelper.swift
+++ NewsFun/NewsHelper.swift
@@ -13,7 +13,23 @@ class NewsHelper {
     
     func getArticles() {
         Alamofire.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=2ecc5647618b4ff393e171fa34438f44").responseJSON { (response) in
-            print(response)
+            if let json = response.result.value as? [String: Any] {
+                if let jsonArticles = json["articles"] as? [[String: Any]] {
+                    var articles = [Article]()
+                    for jsonArticle in jsonArticles {
+                        guard let title = jsonArticle["title"] as? String,
+                            let urlToImage = jsonArticle["urlToImage"] as? String,
+                            let url = jsonArticle["url"] as? String,
+                            let description = jsonArticle["description"] as? String else { continue }
+                        let article = Article()
+                        article.title = title
+                        article.urlToImage = urlToImage
+                        article.url = url
+                        article.description = description
+                        articles.append(article)
+                    }
+                }
+            }
         }
     }
     
